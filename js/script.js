7ab227abdc6cdd20ec3c1ba6b01369c6c32137e0
vladimir <vova.miron.2@gmail.com>
2022-04-18 23:53:26 +0300

    trim string variabile

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 let numberOfFilms;
 
 function start(){
-    numberOfFilms = +prompt("How much movies do you watch?", " ");
+    numberOfFilms = +prompt("How much movies do you watch?", " ").trim();
 
     while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
         numberOfFilms = +prompt("How much movies do you watch?", " ");
@@ -23,8 +23,8 @@ const personalMovieDB = {
 
 function rememberMyFilms(){
     for(let i = 0; i < 2; i++){
-        const nameMovie = prompt("Last film which you watch"),
-              markMovie = prompt("Give a mark for movie");
+        const nameMovie = prompt("Last film which you watch").trim(),
+              markMovie = prompt("Give a mark for movie").trim();
 
         if (nameMovie != null && markMovie != null && nameMovie != "" && markMovie != "" && nameMovie.length < 50){
             personalMovieDB.movies[nameMovie] = markMovie;
@@ -78,3 +78,4 @@ function writeYourGenres(){
 }
 
 console.log(writeYourGenres());
+
